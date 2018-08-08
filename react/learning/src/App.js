4fa925d7c140728857a5bd4533cf853c03aca52d
André Matiazzo <matiazzoandre@gmail.com>
2018-08-08 20:48:13 -0300

    mais tentativa de arrumar analytics

--- react/learning/src/App.js
+++ react/learning/src/App.js
@@ -8,18 +8,18 @@ class App extends Component {
 	state  = {
 		// array
 		persons: [
-			{ nome: 'Andre', idade: 27},
-			{ nome: 'Giove', idade: 26}
+		{ nome: 'Andre', idade: 27},
+		{ nome: 'Giove', idade: 26}
 		]
 	}
 
-switchNameHandler = () => {
+	switchNameHandler = (newName) => {
 	// console.log('Foi clicado');
-	// funciona com state, que só funciona com extends Component
+	// funciona com state, que só funciona com 'extends Component'
 	this.setState({
 		persons: [
-			{ nome: 'Eu', idade: 27},
-			{ nome: 'Giove', idade: 21}
+		{ nome: newName, idade: 27},
+		{ nome: 'Giove', idade: 21}
 		]
 	});
 }
@@ -31,16 +31,21 @@ switchNameHandler = () => {
 			<div className="App">
 			<h1>Oie</h1>
 			<p>Funfa</p>
-			<button onClick={this.switchNameHandler} >Trocar nome</button>
-			<Person name={this.state.persons[0].nome} age={this.state.persons[0].idade}/>
-			<Person name={this.state.persons[1].nome} age={this.state.persons[1].idade}>e gosto de cavalos</Person>
+			<button onClick={() => this.switchNameHandler('Deds')} >Trocar nome</button>
+			<Person 
+			name={this.state.persons[0].nome}
+			age={this.state.persons[0].idade}/>
+			<Person
+			name={this.state.persons[1].nome}
+			age={this.state.persons[1].idade}
+			click={this.switchNameHandler.bind(this, 'Dedeco')}>e gosto de cavalos</Person>
 			</div>
 			);
 		// return React.createElement('div', {className: 'App'}, React.createElement('h1', null, 'Funciona?'));
 	}
 
 	// 01. Em Person.js, criei o componente, a casca
-	// 02. Aqui, os elementos 'html' Person, retomam o que estipulei no componente: os attr name e age
+	// 02. Aqui, os elementos 'html' Person retomam o que estipulei no componente: os attr name e age
 	// 03. Daí, cada um desses attr é definido no objeto pai (App) e referenciado dentro dos elementos 'html'
 	// 04. Então: o componente (Person.js) pega da referência de "aqui vai o componente" (<Person/>) que, por sua vez, pega do objeto pai (App)
 }
--- react/learning/src/Person/Person.js
+++ react/learning/src/Person/Person.js
@@ -6,13 +6,14 @@ import React from 'react';
 // (): lista de argumentos (vazia, nesse caso)
 // => : arrow function
 // props: "allow you to pass data from a parent (wrapping) component to a child (embedded) component."
-const person = (props) => {
+const person = ( props ) => {
 	return (
 		<div>
-			<p>Sou {props.name} e tenho {props.age} anos <span>{props.children}</span></p>
+			<p onClick={props.click}>Sou {props.name} e tenho {props.age} anos <span>{props.children}</span></p>
 		</div>
 		)
 };
+// não muda o state da aplicação
 
 // quando outro arquivo quiser importar esse arquivo de uma forma genérica
 // vai importar person como default
